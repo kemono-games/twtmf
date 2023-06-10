0b7fd3649305796b87c339cc4e0f26e98b91db73
Jesse Zhu <jesse@itjesse.com>
2023-06-11 04:51:42 +0900

    clean

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,13 +3,4 @@ import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const {
-  NODE_ENV,
-  PORT,
-  LOG_FORMAT,
-  LOG_DIR,
-  ORIGIN,
-  OPENAI_API_KEY,
-  OPENAI_PROXY,
-  OPENAI_BASE_URL,
-} = process.env;
+export const { NODE_ENV, PORT, LOG_FORMAT, LOG_DIR } = process.env;
